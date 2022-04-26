4c44f45f241e5c7a8d8f2b3cb83c861133187f18
Burak Ozdemir <ozdemirburak1912@gmail.com>
2022-04-26 11:38:57 +0300

    head

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ function App() {
   return (
     <div className="App">
       <nav>
-        <h1>Wordle React</h1>
+        <h1>Wordle Clone React</h1>
       </nav>
       <AppContext.Provider
         value={{
